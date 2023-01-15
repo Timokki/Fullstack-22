26bc5f97d03c929e1de284f5d9d53656471701ae
Timo Keränen <timo@timokeranen.com>
2023-01-15 14:59:00 +0200

    Unicafe step 6 refactoroitu

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -1,24 +1,22 @@
 import { useState } from "react"
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>
-}
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
 
-const StaticsTable = (props) => {
-  const {good, neutral, bad} = props
-
-  return (
-    <table>
-      <tbody>
-        <StaticTableRow text='Good' value={good} />
-        <StaticTableRow text='Neutral' value={neutral} />
-        <StaticTableRow text='Bad' value={bad} />
-        <StaticTableRow text='All' value={good + neutral + bad} />
-        <StaticTableRow text='Average' value={(good - bad)/(bad + neutral + good)} />
-        <StaticTableRow text='Positive' value={(good / (bad + neutral + good))*100} />
-      </tbody>
-    </table>
-  )
+const StaticsTable = ({good, neutral, bad}) => {
+  if (good > 0 || neutral > 0 || bad > 0){
+    return (
+      <table>
+        <tbody>
+          <StaticTableRow text='Good' value={good} />
+          <StaticTableRow text='Neutral' value={neutral} />
+          <StaticTableRow text='Bad' value={bad} />
+          <StaticTableRow text='All' value={good + neutral + bad} />
+          <StaticTableRow text='Average' value={(good - bad)/(bad + neutral + good)} />
+          <StaticTableRow text='Positive' value={(good / (bad + neutral + good))*100} />
+        </tbody>
+      </table>
+    )
+  }
 }
 
 const StaticTableRow = (props) => {
@@ -42,24 +40,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const setToGood = (newValue) => {
-    setGood(good + 1)
-  }
-
-  const setToNeutral = (newValue) => {
-    setNeutral(neutral + 1)
-  }
-  
-  const setToBad = (newValue) => {
-    setBad(bad + 1)
-  }
-
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={setToGood} text="good" />
-      <Button handleClick={setToNeutral} text="neutral" />
-      <Button handleClick={setToBad} text="bad" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <br/>
       <h1>statistics</h1>
       <StaticsTable good={good} neutral={neutral} bad={bad} />
