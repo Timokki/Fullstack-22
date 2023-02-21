cff310879a414cfa7da9b7e97ac5495e2f1ffba6
Timo Keränen <timo@timokeranen.com>
2023-02-21 15:02:02 +0200

    Puhelinluettelo step 4

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: "040-123 1244"}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameInputChange = (event) => {
     setNewName(event.target.value)
@@ -14,6 +17,11 @@ const App = () => {
 
   const handleNumberInputChahge = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterInputChange = (event) => {
+    setNewFilter(event.target.value)
+    console.log(`Filtteröinti` + filttered(persons, newFilter))
   }
 
   const addNewName = (event) => {
@@ -37,20 +45,28 @@ const App = () => {
     setNewNumber('')
   }
 
+  const filttered = (arr, filter) =>{
+    if (filter !== '')
+      return arr.filter((element) => element.name.toLowerCase().includes(filter.toLowerCase()))
+    
+    return arr
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter show with <input value={newFilter} onChange={handleFilterInputChange}/></div>
       <form>
-        <div>
-          name: <input value={newName} onChange={handleNameInputChange}/>
-        </div>
+        
+        <h2>add a new</h2>
+        <div>name: <input value={newName} onChange={handleNameInputChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberInputChahge} /></div>
         <div>
           <button onClick={addNewName} type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <ShowPerson key={person.name} person={person} />)}
+      {filttered(persons, newFilter).map(person => <ShowPerson key={person.name} person={person} />)}
     </div>
   )
 
