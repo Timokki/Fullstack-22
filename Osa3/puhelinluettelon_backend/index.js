bc8f1a491ebdd8115a661a320eb440a4133272af
Timo Keränen <timo@timokeranen.com>
2023-05-12 14:59:44 +0300

    3.17*: puhelinluettelo ja tietokanta, step5

--- Osa3/puhelinluettelon_backend/index.js
+++ Osa3/puhelinluettelon_backend/index.js
@@ -73,6 +73,21 @@ app.post('/api/persons', (req,res) =>{
   .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (req, res, next) =>{
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {res.status(204).end()})
